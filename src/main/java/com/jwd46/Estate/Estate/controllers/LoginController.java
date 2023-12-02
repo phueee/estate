f2514d475ed29dbc316e20fb7ded3fa14bdd8244
Pyo112 <ppaw7066@gmail.com>
2023-12-02 22:16:22 +0630

    aaa

--- src/main/java/com/jwd46/Estate/Estate/controllers/LoginController.java
+++ src/main/java/com/jwd46/Estate/Estate/controllers/LoginController.java
@@ -6,9 +6,11 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
 
 @Controller
 public class LoginController {
@@ -20,17 +22,6 @@ public class LoginController {
         model.addAttribute("title","login");
         return "login";
     }
-    @PostMapping("/login")
-    public String showLoginGet(Model model, @RequestParam String userEmail, String userPassword) {
-        User user = new User();
-        user.setUserEmail(userEmail);
-        user.setUserPassword(userPassword);
-        if (user.getUserEmail().equals("admin@gmail.com") && user.getUserPassword().equals("12334566")) {
-            return "buy";
-        } else {
-            return "login";
-        }
 
-    }
 
 }
