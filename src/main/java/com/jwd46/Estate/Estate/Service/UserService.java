dfa7fbf16db34a06b03fa5ce9b883b59d398bced
phueee <phuesone05@gmail.com>
2023-12-02 12:03:51 +0630

    tin p

---
+++ src/main/java/com/jwd46/Estate/Estate/Service/UserService.java
@@ -0,0 +1,17 @@
+package com.jwd46.Estate.Estate.Service;
+
+import com.jwd46.Estate.Estate.daos.UserDao;
+import com.jwd46.Estate.Estate.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserDao userDao;
+    public User login(String email, String password) {
+
+        return userDao.findByEmailAndPassword(email, password);
+    }
+
+}
