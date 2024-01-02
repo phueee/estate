fc105ca453ca8b32e42f63560891ba01e4cf2a3b
sandizaw <zinnia959704@gmail.com>
2024-01-02 20:07:44 +0630

    abcde

---
+++ src/main/java/com/jwd46/Estate/Estate/models/UserHomeRent.java
@@ -0,0 +1,26 @@
+package com.jwd46.Estate.Estate.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+
+@Table(name = "userhomerent")
+@Data
+public class UserHomeRent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "home_id", referencedColumnName = "homeId")
+    private Home home;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+}
